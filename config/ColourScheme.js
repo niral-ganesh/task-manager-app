cd4dab3d0428ec896434bc7f0b848ffa7895c651
Niral G <139200338+niral-ganesh@users.noreply.github.com>
2025-03-31 03:50:51 +0800

    Create ColourScheme.js

---
+++ config/ColourScheme.js
@@ -0,0 +1,14 @@
+// Define colour scheme
+export default {
+    blue1: "#03045e",
+    blue2: "#0077b6",
+    blue3: "#00b4d8",
+    blue4: "#90e0ef",
+    blue5: "#caf0f8",
+    black: "#000000",
+    white: "#ffffff",
+    lightgrey: "#d4d5d6",
+    mediumgrey: "#b3b3b3",
+    darkgrey: "#7e7f80",
+    red: "red",
+}
